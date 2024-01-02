2f11ad9615d1243c0b858343a1e4b5fa4a5181f9
Mini256 <minianter@foxmail.com>
2024-01-02 23:15:14 +0800

    feat: update the openapi docs and README.md

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@ import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
 
 export const router = OpenAPIRouter({
 	docs_url: "/",
+	
+	schema: {
+		info: {
+			title: "TiDB Serverless API",
+			description: "The API to access TiDB Serverless",
+			version: "0.0.1",
+		},
+		servers: [
+			{
+				url: "https://tidb-serverless-cf.minianter.workers.dev/",
+				description: "Cloudflare Worker Access Endpoint",
+			},
+		],
+	}
 });
 
 router.post("/api/query/", ExecuteQuery);
